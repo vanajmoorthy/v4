3635fb1954bf1f5533a0531fffef1c3a8e4fee26
vanajmoorthy <vanajmoorthy@gmail.com>
2020-04-11 01:23:17 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,115 @@
+// eslint-disable
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">
+				<span id="fulltext">
+					<span id="vanaj">Vanaj</span>
+					<span id="moorthy"> Moorthy</span>
+				</span>
+				<span id="shorttext">
+					<span id="vanaj">V</span>
+					<span id="moorthy">M</span>
+				</span>
+			</router-link>
+			<ul>
+				<li><router-link to="/code">Code</router-link></li>
+				<li><router-link to="/work">Work</router-link></li>
+				<li><router-link to="/design">Design</router-link></li>
+			</ul>
+		</div>
+		<router-view />
+	</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+	background-color: #f7fafc;
 }
+</style>
 
-#nav {
-  padding: 30px;
+<style scoped>
+
+#shorttext {
+	display: none;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@media only screen and (max-width: 494px) {
+	#shorttext {
+		display: block;
+	}
+	#fulltext {
+		display: none;
+	}
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	text-decoration: underline !important;
+}
+
+a {
+	text-decoration: none;
+	color: #576980;
+
+}
+
+
+#vanaj {
+	color: #2998c4;
+}
+
+#nav {
+	font-family: canada-type-gibson, sans-serif;
+	font-weight: 600;
+	font-style: normal;
+	max-width: 100vw;
+	height: 17vh;
+	margin: 0 2rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	font-size: 1.3rem;
+}
+
+ul {
+	padding: 0;
+	font-weight: 400;
+	list-style-type: none;
+	margin: 0;
+	cursor: default;
+	text-align: center;
+}
+
+li {
+	padding: 0;
+	margin: 0;
+	display: inline-block;
+	margin-left: 25px;
+	cursor: pointer;
+	user-select: none;
+	transition: all 0.3s ease-out;
+}
+
+@media only screen and (max-width: 385px) {
+	li {
+		margin-left: 17px;
+	}
+}
+
+@media only screen and (max-width: 360px) {
+	li {
+		margin-left: 10px;
+	}
+}
+
+@media only screen and (max-width: 320px) {
+	li {
+		margin-left: 7px;
+	}
+}
+
+a:hover {
+	color: #2998c4;
+	transition: all 0.3s ease;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,24 +5,46 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+	{
+		path: "/",
+		name: "Home",
+		component: Home
+	},
+	{
+		path: "/code",
+		name: "Code",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/Code.vue")
+	},
+	{
+		path: "/work",
+		name: "Work",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/Work.vue")
+	},
+	{
+		path: "/design",
+		name: "Design",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/Design.vue")
+	}
 ];
 
 const router = new VueRouter({
-  routes
+    mode: 'history',
+    hashbang: false,
+	routes
 });
+
+router.mode = 'html5'
 
 export default router;
