72abb1c206769f284ef0a7a5a25905a340df80c7
vanajmoorthy <vanajmoorthy@gmail.com>
2020-04-23 18:36:19 +0530

    made icons links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,50 @@
 <template>
 	<footer class="footer">
 		<div class="icon">
-			<i class="fab fa-behance-square fa-lg"></i>
+			<a href="https://www.behance.net/vanajmoorthy" target="_blank">
+				<i class="fab fa-behance-square fa-lg"></i>
+			</a>
 		</div>
 		<div class="icon">
-			<i class="fas fa-envelope fa-lg"></i>
+			<a href="mailto:vanajmoorthy@gmail.com" target="_blank">
+				<i class="fas fa-envelope fa-lg"></i>
+			</a>
 		</div>
 		<div class="icon">
-			<i class="fab fa-github fa-lg"></i>
+			<a href="https://github.com/vanajmoorthy" target="_blank">
+				<i class="fab fa-github fa-lg"></i>
+			</a>
 		</div>
 		<div class="icon">
-			<i class="fab fa-instagram fa-lg"></i>
+			<a href="https://instagram.com/vanajmoorthy" target="_blank">
+				<i class="fab fa-instagram fa-lg"></i>
+			</a>
 		</div>
 		<div class="icon">
-			<i class="fab fa-twitter fa-lg"></i>
+			<a href="https://twitter.com/vanajmoorthy" target="_blank">
+				<i class="fab fa-twitter fa-lg"></i>
+			</a>
 		</div>
 	</footer>
 </template>
 
 <script>
 export default {
-	name: "Footer"
+	name: "Footer",
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+a {
+  color: #576980
+}
+
+a:hover {
+  color: #2998c4;
+  transition: all 0.3s ease;
+}
 
 .icon {
 	display: inline;
