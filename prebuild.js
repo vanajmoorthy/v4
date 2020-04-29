5945d7641971fe33a975f0f3b9c8fb1408e3788d
vanajmoorthy <vanajmoorthy@gmail.com>
2020-04-29 18:44:58 +0530

    added all sub redirects

---
+++ prebuild.js
@@ -0,0 +1,22 @@
+const apps = require("./apps.json")
+	.map((app) => {
+		return `https://www.vanajmoorthy.com/${app} https://www.vanajmoorthy.com/${app}/index.html 302
+https://www.vanajmoorthy.com/${app}/* https://${app}-vanajmoorthy.netlify.com/:splat 200!`;
+	})
+	.join("\n\n");
+
+const fs = require("fs");
+
+const template = fs.readFileSync("./_redirects-template", "utf-8");
+
+const output = template.replace("$APP_REDIRECTS", apps);
+
+fs.writeFileSync("./public/_redirects", output);
+// https://www.vanajmoorthy.com/morph https://www.vanajmoorthy.com/morph/index.html 302
+// https://www.vanajmoorthy.com/morph/* https://morph-vanajmoorthy.netlify.com/:splat 200!
+
+// https://www.vanajmoorthy.com/obstacles https://www.vanajmoorthy.com/obstacles/index.html 302
+// https://www.vanajmoorthy.com/obstacles/* https://obstacles-vanajmoorthy.netlify.com/:splat 200!
+
+// https://www.vanajmoorthy.com/graph https://www.vanajmoorthy.com/graph/index.html 302
+// https://www.vanajmoorthy.com/graph/* https://graph-vanajmoorthy.netlify.com/:splat 200!
