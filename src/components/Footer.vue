efd2d2235c8c917499b03eda79d89108464169b1
vanajmoorthy <vanajmoorthy@gmail.com>
2020-07-13 21:38:10 +0530

    changed footer styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,33 @@
 <template>
-	<footer class="footer">
-		<div class="icon">
-			<a href="https://www.behance.net/vanajmoorthy" target="_blank">
-				<i class="fab fa-behance-square fa-lg"></i>
-			</a>
-		</div>
-		<div class="icon">
-			<a href="mailto:vanajmoorthy@gmail.com" target="_blank">
-				<i class="fas fa-envelope fa-lg"></i>
-			</a>
-		</div>
-		<div class="icon">
-			<a href="https://github.com/vanajmoorthy" target="_blank">
-				<i class="fab fa-github fa-lg"></i>
-			</a>
-		</div>
-		<div class="icon">
-			<a href="https://instagram.com/vanajmoorthy" target="_blank">
-				<i class="fab fa-instagram fa-lg"></i>
-			</a>
-		</div>
-		<div class="icon">
-			<a href="https://twitter.com/vanajmoorthy" target="_blank">
-				<i class="fab fa-twitter fa-lg"></i>
-			</a>
-		</div>
-	</footer>
+	<div class="wrapper">
+		<footer class="footer">
+			<div class="icon">
+				<a href="https://www.behance.net/vanajmoorthy" target="_blank">
+					<i class="fab fa-behance-square fa-lg"></i>
+				</a>
+			</div>
+			<div class="icon">
+				<a href="mailto:vanajmoorthy@gmail.com" target="_blank">
+					<i class="fas fa-envelope fa-lg"></i>
+				</a>
+			</div>
+			<div class="icon">
+				<a href="https://github.com/vanajmoorthy" target="_blank">
+					<i class="fab fa-github fa-lg"></i>
+				</a>
+			</div>
+			<div class="icon">
+				<a href="https://instagram.com/vanajmoorthy" target="_blank">
+					<i class="fab fa-instagram fa-lg"></i>
+				</a>
+			</div>
+			<div class="icon">
+				<a href="https://twitter.com/vanajmoorthy" target="_blank">
+					<i class="fab fa-twitter fa-lg"></i>
+				</a>
+			</div>
+		</footer>
+	</div>
 </template>
 
 <script>
@@ -36,14 +38,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 a {
-  color: #576980
+	color: #576980;
 }
 
 a:hover {
-  color: #2998c4;
-  transition: all 0.3s ease;
+	color: #2998c4;
+	transition: all 0.3s ease;
 }
 
 .icon {
@@ -57,12 +58,20 @@ a:hover {
 	}
 }
 
+.wrapper {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
 .footer {
 	/* backdrop-filter: blur(2px); */
-	padding-top: 40px;
-	/* position: fixed; */
+	padding: 10px 0 20px 0;
+	display: flex;
+	/* flex: 0.5; */
+	justify-content: space-evenly;
 	height: 9vh;
-	width: 100%;
+	width: 30%;
 	color: #576980;
 	text-align: center;
 }
