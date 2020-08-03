76beb7d0d0d1c0d20e615d174b5246fb4ba58356
vanajmoorthy <vanajmoorthy@gmail.com>
2020-08-03 23:51:11 +0530

    remove scrollbar

--- src/App.vue
+++ src/App.vue
@@ -54,19 +54,7 @@ body {
 
 /* scrollbar */
 ::-webkit-scrollbar {
-	width: 5px;
-}
-
-::-webkit-scrollbar-track {
-	background: #f1f1f1;
-}
-
-::-webkit-scrollbar-thumb {
-	background: #576980;
-}
-
-::-webkit-scrollbar-thumb:hover {
-	background: #2998c4;
+	width: 0px;
 }
 </style>
 
